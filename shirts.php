ad5ec25ac33fd40d71d30223cae96fda52ac38e5
Manuel Ernesto <manuelernesto@outlook.com>
2016-02-10 09:28:41 +0100

    Add an list of shirts

--- shirts.php
+++ shirts.php
@@ -1,6 +1,4 @@
 <?php
-$pageTitle = "Mike's Full Catalog of Shirts";
-$section = "shirts";
 /**
  * Created by PhpStorm.
  * User: manuelernesto
@@ -8,14 +6,72 @@ $section = "shirts";
  * Time: 4:56 PM
  */
 
-include('inc/header.php'); ?>
+$pageTitle = "Mike's Full Catalog of Shirts";
+$section = "shirts";
+include('inc/header.php');
 
-    <div class="section page">
-        <h1>Mike&rsquo;s Full Catalog of Shirts</h1>
+
+$products = array();
+$products[101] = array(
+    "name" => "Logo Shirt, Red",
+    "img" => "img/shirts/shirt-101.jpg",
+    "price" => 18
+);
+$products[102] = array(
+    "name" => "Mike the Frog Shirt, Black",
+    "img" => "img/shirts/shirt-102.jpg",
+    "price" => 20
+);
+$products[103] = array(
+    "name" => "Mike the Frog Shirt, Blue",
+    "img" => "img/shirts/shirt-103.jpg",
+    "price" => 20
+);
+$products[104] = array(
+    "name" => "Logo Shirt, Green",
+    "img" => "img/shirts/shirt-104.jpg",
+    "price" => 18);
+$products[105] = array(
+    "name" => "Mike the Frog Shirt, Yellow",
+    "img" => "img/shirts/shirt-105.jpg",
+    "price" => 25
+);
+$products[106] = array(
+    "name" => "Logo Shirt, Gray",
+    "img" => "img/shirts/shirt-106.jpg",
+    "price" => 20
+);
+$products[107] = array(
+    "name" => "Logo Shirt, Turquoise",
+    "img" => "img/shirts/shirt-107.jpg",
+    "price" => 20
+);
+$products[108] = array(
+    "name" => "Logo Shirt, Orange",
+    "img" => "img/shirts/shirt-108.jpg",
+    "price" => 25,
+);
+
+?>
+
+
+    <div class="section shirts page">
+        <div class="wrapper">
+            <h1>Mike&rsquo;s Full Catalog of Shirts</h1>
+
+            <ul class="products">
+                <?php foreach ($products as $product) {
+                    echo "<li>";
+                    echo "<a href=\"#\">";
+                    echo '<img src="' . $product["img"] . '"  alt="' . $product["name"] . '" > ';
+                    echo "<p> View Details</p>";
+                    echo "</a href=\"#\">";
+                    echo "</li>";
+                } ?>
+            </ul>
+        </div>
+
     </div>
-
-
-
 
 
 <?php include('inc/footer.php');
